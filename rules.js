863e9c6ec99fa8e1fb91f456df10f07af50604d2
Jonathan Reksono <jreksono@gmail.com>
2022-01-18 19:38:34 -0500

    Add game functionality

--- rules.js
+++ rules.js
@@ -14,3 +14,52 @@ function computerPlay() {
   }
   return play;
 }
+
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Paper') {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors') {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Rock') {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Scissors') {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock') {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Paper') {
+    return "You Win! Scissors beats Paper";
+  } else {
+    return "It's a Tie!";
+  }
+}
+
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let result = '';
+  let playerPlay = '';
+  for (let i = 0; i < 5; i++) {
+    do {
+      playerPlay = window.prompt('Enter rock, paper, or scissors.');
+    } while (!(playerPlay.toLowerCase() == 'rock' || playerPlay.toLowerCase() == 'paper' || playerPlay.toLowerCase() == 'scissors'))
+    result = playRound(playerPlay, computerPlay());
+    console.log(result);
+    if (result.includes("Win!")) {
+      playerScore += 1;
+    } else if (result.includes("Lose!")) {
+      computerScore += 1;
+    }
+  }
+  console.log(`Player: ${playerScore} | Computer: ${computerScore}`);
+  if (playerScore > computerScore) {
+    console.log("You Won!");
+  } else if (playerScore < computerScore) {
+    console.log("You Lose!");
+  } else {
+    console.log("It's a tie!");
+  }
+}
+
+game();
